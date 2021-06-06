45105696a3c0d51ce45e33bc6e6ee4896b854126
IlayTissona <ilay211@gmail.com>
2021-06-06 13:19:04 +0300

    all links endpoint

--- crawler/index.js
+++ crawler/index.js
@@ -1,10 +1,25 @@
 const cheerio = require("cheerio");
 const tr = require("tor-request");
+const app = require("express")();
 
-async function main() {
+app.get("/all-links", async (req, res) => {
+  const allLinks = await getAllLinks();
+  res.json(allLinks);
+});
+
+app.get("/all-links", async (req, res) => {
+  const allLinks = await getAllLinks();
+  res.json(allLinks);
+});
+
+app.listen(8080, () => console.log("listening on 80"));
+
+//------------------------------------------------------functions------------------------------------------------------
+
+async function getAllLinks() {
   const allLinks = [];
-
   let nextPage = true;
+
   for (let i = 1; i < 100 && nextPage; i++) {
     try {
       const pageLinks = await searchPage(i);
@@ -14,9 +29,9 @@ async function main() {
       nextPage = false;
     }
   }
-  console.log(allLinks);
+
+  return allLinks;
 }
-main();
 
 async function searchPage(page) {
   return new Promise((resolve, reject) => {
