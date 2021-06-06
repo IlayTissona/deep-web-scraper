98f0ecd9ae6d2afaa3772a5a88553c3331768a2c
IlayTissona <ilay211@gmail.com>
2021-06-06 21:49:17 +0300

    basic endpoints

--- server/index.js
+++ server/index.js
@@ -4,11 +4,30 @@ const PORT = 80;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  db.query("SELECT * FROM `pastes`", (err, results, fields) => {
+app.get("/all-pastes", (req, res) => {
+  db.query("SELECT * FROM `pastes`", (err, results) => {
     if (err) return res.json(err);
     res.json(results);
   });
+});
+
+app.get("/all-authors", (req, res) => {
+  db.query("SELECT `author` FROM `pastes`", (err, results) => {
+    if (err) return res.json(err);
+    res.json(results);
+  });
+});
+
+app.get("/author/:authorName", (req, res) => {
+  const { authorName } = req.params;
+  db.query(
+    "SELECT * FROM `pastes` WHERE author = ?",
+    [authorName],
+    (err, results) => {
+      if (err) return res.json(err);
+      res.json(results);
+    }
+  );
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
