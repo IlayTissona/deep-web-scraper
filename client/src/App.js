33625a91baab0ede0e31eafb29f8d6e53e6f3fd2
IlayTissona <ilay211@gmail.com>
2021-06-07 13:19:21 +0300

    header + little styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
+import "./styles/App.css";
 import PastesTable from "./components/PastesTable";
 
 function App() {
   return (
     <div className="App">
       <PastesTable />
+      <div id="header">
+        <h3>Stronghold Scraper</h3>
+        <h4>NOTE</h4>
+        <div>
+          This website shows content from a deep/dark web paste holder. Pastes
+          may contain illegal, or even worse than just illegal content. Be
+          responsible, chose the right path.
+        </div>
+      </div>
     </div>
   );
 }
