cf13eb73ae929faa87aa70215e7b87e14ca64b99
IlayTissona <ilay211@gmail.com>
2021-06-10 00:15:53 +0300

    entities filter

--- client/src/components/FiltersBar.js
+++ client/src/components/FiltersBar.js
@@ -1,11 +1,64 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "../styles/FiltersBar.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function FiltersBar({ setList, setScrollable }) {
   const [searchKey, setKey] = useState("text");
   const searchCancelToken = useRef();
+
+  const [entitiesState, setEntitiesState] = useState({
+    entities: [],
+    hasMore: true,
+    offset: 0,
+    chosen: null,
+  });
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/entities?limit=20`)
+      .then(({ data: entities }) =>
+        setEntitiesState((prev) => ({ ...prev, entities }))
+      );
+  }, []);
+
+  const moreEntities = () => {
+    axios
+      .get(
+        `http://localhost:3000/entities?limit=20&offset=${entitiesState.offset}`
+      )
+      .then((res) => {
+        if (!res.data.length)
+          return setEntitiesState({ ...entitiesState, hasMore: false });
+
+        const entities = [...entitiesState.entities, ...res.data];
+        console.log(entities);
+        setEntitiesState({
+          ...entitiesState,
+          entities,
+          offset: entities.length,
+        });
+      });
+  };
+
+  const entityClickHandler = (entity) => {
+    if (entitiesState.chosen === entity) {
+      axios.get("http://localhost:3000/entities").then(({ data: entities }) => {
+        setEntitiesState({ ...entitiesState, entities, chosen: null });
+      });
+      return setFullList();
+    }
+    axios.get("http://localhost:3000/entity/" + entity).then((res) => {
+      console.log(res.data);
+      setEntitiesState({
+        ...entitiesState,
+        entities: [{ entity }],
+        chosen: entity,
+      });
+      setList(res.data);
+    });
+  };
 
   const setFullList = () => {
     axios
@@ -55,8 +108,38 @@ function FiltersBar({ setList, setScrollable }) {
           </button>
         </div>
       </div>
+      <div
+        id="entities"
+        className={`${entitiesState.chosen ? "no-scroll" : ""}`}
+      >
+        <InfiniteScroll
+          hasMore={entitiesState.hasMore}
+          dataLength={entitiesState.entities.length}
+          next={moreEntities}
+          scrollableTarget="entities"
+          loader={
+            <div className="entity last">
+              <p>Loading More...</p>
+            </div>
+          }
+          endMessage={<div></div>}
+        >
+          {entitiesState.entities && entitiesState.entities?.map(makeEntity)}
+        </InfiniteScroll>
+      </div>
     </div>
   );
+  function makeEntity({ entity }) {
+    return (
+      <div
+        className={`entity${entitiesState.chosen === entity ? " chosen" : ""}`}
+        key={entity}
+        onClick={() => entityClickHandler(entity)}
+      >
+        <p>{entity}</p>
+      </div>
+    );
+  }
 }
 
 export default FiltersBar;
